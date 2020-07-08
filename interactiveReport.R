0dd0465ff95cd02f693c753a466192f415faf93a
Ali Fradi <Ali.Fradi@axefinance.com>
2020-07-08 17:09:07 +0100

    first commit

---
+++ interactiveReport.R
@@ -0,0 +1,22 @@
+library(shiny)
+
+## Part I ------------------------------
+
+ui <- fluidPage(
+
+    
+)
+
+
+
+## Part II ------------------------------
+
+server <- function(input, output) {
+
+
+}
+
+
+# Section Run Application ------------------------------
+
+shinyApp(ui = ui, server = server)
